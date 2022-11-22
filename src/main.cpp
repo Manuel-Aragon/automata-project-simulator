e2548bf5065b5427644ec1ef76f8ea1192e1b3f8
Manuel Aragon <aragon.manuel@live.com>
2022-11-22 12:59:59 -0600

    outlined program

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
+#include <vector>
+
+
+//2d grid
+struct grid{
+    //vector to hold grid
+
+    //function returns true if coordinate is occupied
+
+    //places character in square
+
+    //removes character from square
+
+    //moves character form one square to another
+
+    //display grid to console;
+};
+
+//character
+class character{
+    //health
+    //action
+};
+
+
+void simulate(){
+    //anounce simulation start
+    std::cout << "Simulation Start";
+
+    //state machine
+        //Start State
+            //establish positions
+            //roll initative
+        //Combat
+            //take turns until one is dead
+        //End State
+            //declare winner
+            //clean up
+
+    //announce simulation end
+    std::cout << "Simulation Complete";
+}
 
 int main(){
-    std::cout << "Hello World";
+    simulate();
 }
